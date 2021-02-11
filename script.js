7a82f39410606ea31e077068e3744540fe64549d
lucasmetron <lucasmetron@gmail.com>
2021-02-11 16:22:28 -0300

    arrays

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-var numero = parseInt(Math.random() *100);
+/*var alunos = ["igor", "jose", "marcos", "mariana"];
+console.log(alunos);
+console.log(alunos.length)
 
-while (numero < 90){
+var aluno1 = "igor";
+var aluno2 = "jose";
+var aluno3 = "marcos";
+var aluno4 = "mariana";
+var aluno5 = "joana";
 
-    console.log(numero);
-    numero = parseInt(Math.random() *100);
+console.log(aluno1);
+console.log(aluno2);
+console.log(aluno3);
+console.log(aluno4);
+console.log(aluno5);
+
+var alunos = ["igor", "jose", "marcos", "mariana", "joana" ];
+
+console.log(alunos[0]);
+console.log(alunos[1]);
+console.log(alunos[2]);
+console.log(alunos[3]);
+console.log(alunos[4]);
+
+
+
+var alunos = ["igor", "jose", "marcos", "mariana", 
+"joana", "Lucas", "lais", "selina" ];
+
+for(var i = 0; i < alunos.length; i++){
+
+    console.log(alunos[i]);
+
 }
-console.log(numero);
-console.log("acabou");
+
+
+var alunos = ["igor", "jose", "marcos", "mariana", 
+"joana", "Lucas", "lais", "selina" ];
+
+for(var i in alunos){
+
+    console.log(alunos[i]);
+}
+
+*/
+
+var alunos = ["igor", "jose", "marcos", "mariana", 
+"joana", "Lucas", "lais", "selina" ];
+
+for(var alunos of alunos){
+
+    console.log(alunos);
+}
+
+

8a1ceac479d6633942a20498d5cd56e489472c4b
lucasmetron <lucasmetron@gmail.com>
2021-02-11 14:09:28 -0300

    estudando IF

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var idade = parseFloat(prompt("Digite sua idade:"));
+
+
+
+if ( idade >= 15 && idade <=17){
+    console.log("Tem direito a desconto");
+} 
+else if ( idade >= 18 && idade <= 30){
+    console.log("Tem direito a desconto porem tem que mostrar a carteirinha");  
+} else {
+    console.log("nao tem desconto");
+}
+
+
+

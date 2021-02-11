b1a4e19cd12d061c65dacc07de58563866e8196f
lucasmetron <lucasmetron@gmail.com>
2021-02-11 15:46:09 -0300

    for e while

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-var nota1 = 6;
-var nota2 = 7;
+var numero = parseInt(Math.random() *100);
 
-var media = (nota1 + nota2) / 2;
+while (numero < 90){
 
-var conceito = "";
-
-if( media >= 8){
-    conceito ="òtimo";
-
-} else if (media >= 6.5 ) {
-    conceito ="Bom";
+    console.log(numero);
+    numero = parseInt(Math.random() *100);
 }
-
-else {
-    conceito ="Regular";
-}
-
-console.log(conceito);
-console.log(media);
-
-switch(conceito){
-
-    case "òtimo":
-        console.log("Parabéns, vc é um otimo aluno");
-        break;
-
-    case "Bom":
-        console.log("Bom, Vc pode melhorar!");
-        break;
-
-    case "Regular":
-        console.log("A nota foi ruim, vc precisa melhorar")
-        break;
-
-    default: 
-        console.log ("houve algum erro");
-        break;
-
-}
+console.log(numero);
+console.log("acabou");

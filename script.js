1f827e677246ccb5020dcbd7f59ffbb465649247
lucasmetron <lucasmetron@gmail.com>
2021-02-11 14:56:48 -0300

    switch JS

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
+var nota1 = 6;
+var nota2 = 7;
 
-var idade = parseFloat(prompt("Digite sua idade:"));
+var media = (nota1 + nota2) / 2;
 
+var conceito = "";
 
+if( media >= 8){
+    conceito ="òtimo";
 
-if ( idade >= 15 && idade <=17){
-    console.log("Tem direito a desconto");
-} 
-else if ( idade >= 18 && idade <= 30){
-    console.log("Tem direito a desconto porem tem que mostrar a carteirinha");  
-} else {
-    console.log("nao tem desconto");
+} else if (media >= 6.5 ) {
+    conceito ="Bom";
 }
 
+else {
+    conceito ="Regular";
+}
 
+console.log(conceito);
+console.log(media);
 
+switch(conceito){
+
+    case "òtimo":
+        console.log("Parabéns, vc é um otimo aluno");
+        break;
+
+    case "Bom":
+        console.log("Bom, Vc pode melhorar!");
+        break;
+
+    case "Regular":
+        console.log("A nota foi ruim, vc precisa melhorar")
+        break;
+
+    default: 
+        console.log ("houve algum erro");
+        break;
+
+}

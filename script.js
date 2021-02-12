65505aa6557fd74c3a0e4e1e1500b0ecaeb7d776
lucasmetron <lucasmetron@gmail.com>
2021-02-12 16:32:56 -0300

    aprendendo e brincando com objeto

--- script.js
+++ script.js
@@ -1,98 +1,32 @@
-//Nome do aluno - Nota 1 - Nota 2 - Media - Aprovado ou reprovado
-/*function media(n1, n2){
-    return (n1+ n2) / 2;
-}
-
-function seletorAluno(nome){
-
-    var alunoselect;
-
-    for (i=0; i <= nomes.length; i++) {
-
-        if(nome != nomes[i] ){
-        } 
-        
-        else if (nome == nomes[i]) {
-            
-            alunoselect = nomes[i];
-        }
-        else if(nomes.leght -1 > i){
-            alunoselect = "Desconhecido"
-        }
-
-    }
+var aluno = {
+    nome: "lucas",
+    idade: 24,
+    notas: [8,6,7,4],
+    responsaveis: [
+        {
+        nome: "Selina",
+        idade: 58,
+    },
     
-   
-
-    return alunoselect;
-}
-
-
-
-//console.log(media(7,8));
-console.log (seletorAluno("Bianca"));*/
-
-//Nome do aluno - Nota 1 - Nota 2 - Media - Aprovado ou reprovado
-
-var nomes = ["Lucas", "Jose", "Maria"];
-var notasA = [7.0, 6.5, 9.5];
-var notasB = [8.0, 7.0, 8.5];
-
-function media (n1 ,n2){
-    return (n1+n2)/2;
-}
-
-function passou(media){
-
-
-    if (media > 7){
-        return "Aprovado!"
-    } else {
-        return "Reprovado!"
+    {
+        nome: "Manoel",
+        idade: 60,
     }
 
+]  
+
 }
 
-/*function juiz (m){
+var media = null;
 
-    var status = "vazio"; 
-    
-    switch (m){
-
-        case m >= 6:
-            status = "Aprovado!" 
-        break;
-
-        case m >=5.9:
-            status = "Recuperação"
-        break;
-
-        case m < 5:
-            status = "Reprovado"
-            break;
-        
-        default:
-             console.log("Erro!");
-             break;
-
-    }       
-    
-    return status;
-}*/
-
-for (var index in nomes){
-
-    console.log(
-    nomes[index] + " " +
-    notasA[index] + " " + 
-    notasB[index] + " " +
-    "Sua média foi: "+ media(notasA[index], notasB[index]) + 
-    " Você está: " + passou(media(notasA[index], notasB[index]))
-    
-    );
+for ( i = 0; i < aluno.notas.length; i++){
+    media = media + aluno.notas[i];
 }
 
+aluno.media = media / aluno.notas.length;
 
+
+console.log(aluno.media);
 
 
 

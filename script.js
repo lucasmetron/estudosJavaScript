e10219c13f57349c64cb39647433e7b8181ec17c
lucasmetron <lucasmetron@gmail.com>
2021-02-14 01:28:36 -0300

    relembrando

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
-var aluno = {
+var idade = parseFloat(prompt("Digite sua idade:"));
+ 
+ 
+ 
+if (idade >= 18 && idade <= 35) {
+    console.log("pode!");
+    console.log("mostre sua indentidade!");
+}
+ 
+else if ( idade > 35){
+    console.log("Qual seu pedido?");
+}
+ 
+else {
+    console.log("não pode você tem menos de 18 anos");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var aluno = {
     nome: "lucas",
     idade: 24,
     notas: [8,6,7,4],
@@ -27,7 +71,7 @@ aluno.media = media / aluno.notas.length;
 
 
 console.log(aluno.media);
-
+*/
 
 
 

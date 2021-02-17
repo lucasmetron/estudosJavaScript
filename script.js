57a99f3cf1d9e1f4ce664f0da0c1cbaa3f11e077
lucasmetron <lucasmetron@gmail.com>
2021-02-17 11:41:26 -0300

    objeto - metodos

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-var alunos = {
-nome: "lucas",
-idade: 24,
-notas: [6,8,9],
-responsaveis: [{
-        nome: "selina",
-        idade:45
-}, {
-    nome:"manoel"
+function calcmedia (){
+    return (this.notas[0] + this.notas[1]) / 2
 }
 
-]
+var aluno = {
+    nome:"Igor",
+    notas: [7,8],
+    media: calcmedia
 
 }
+var aluno1 = {
+    nome:"Lucas",
+    notas: [9, 5],
+    media: calcmedia
+    }
 
 
+console.log(aluno);
 
-console.log(alunos.responsaveis[0])
+console.log(aluno.nome);
+console.log(aluno.media());
+
+console.log(aluno1.nome)
+console.log(aluno1.media())

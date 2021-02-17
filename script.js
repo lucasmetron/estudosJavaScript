bcb1f031f493da9b0fe9c8dca0df4f001463f677
lucasmetron <lucasmetron@gmail.com>
2021-02-17 11:04:26 -0300

    revisando

--- script.js
+++ script.js
@@ -1,77 +1,18 @@
-var idade = parseFloat(prompt("Digite sua idade:"));
- 
- 
- 
-if (idade >= 18 && idade <= 35) {
-    console.log("pode!");
-    console.log("mostre sua indentidade!");
+var alunos = {
+nome: "lucas",
+idade: 24,
+notas: [6,8,9],
+responsaveis: [{
+        nome: "selina",
+        idade:45
+}, {
+    nome:"manoel"
 }
- 
-else if ( idade > 35){
-    console.log("Qual seu pedido?");
-}
- 
-else {
-    console.log("não pode você tem menos de 18 anos");
+
+]
+
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var aluno = {
-    nome: "lucas",
-    idade: 24,
-    notas: [8,6,7,4],
-    responsaveis: [
-        {
-        nome: "Selina",
-        idade: 58,
-    },
-    
-    {
-        nome: "Manoel",
-        idade: 60,
-    }
-
-]  
-
-}
-
-var media = null;
-
-for ( i = 0; i < aluno.notas.length; i++){
-    media = media + aluno.notas[i];
-}
-
-aluno.media = media / aluno.notas.length;
-
-
-console.log(aluno.media);
-*/
-
-
-
+console.log(alunos.responsaveis[0])

e1ff4ef90869987734202b981af6bf3818cb8f7b
lucasmetron <lucasmetron@gmail.com>
2021-02-18 10:37:45 -0300

    exercicio calculo de media

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+    EXERCICIO 1 
 var letra = "z";
 
 if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"  ){
@@ -6,6 +8,20 @@ if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"
  else {
     console.log("Letra ("+letra+") é uma consoante!")
  }
+*/
 
+
+var aluno = "Lucas";
+var nota1 = 7;
+var nota2 = 6;
+var media = (nota1+nota2)/2;
+
+if (media == 10 ){
+    alert( aluno + " Aprovado com distinção "+media)
+} else if (media >= 7 ){
+    alert( aluno + " Aprovado " +media)
+} else {
+    alert( aluno + " Reprovado " +media)
+}
  
 

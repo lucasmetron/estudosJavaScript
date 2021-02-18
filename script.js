4761195f688f5e1fdf6c98a9bc81a020387b7efa
lucasmetron <lucasmetron@gmail.com>
2021-02-18 11:32:14 -0300

    EXERCICIO MAIOR E MENOR DE 3 NUMEROS

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ if (n1 > n2 && n1 > n3){
 
 */
 
-
+/*
     //EXERCICIO 4
 var n1 = prompt("digite o numero 1 : ");
 var n2 = prompt("digite o numero 2 : ");
@@ -57,4 +57,36 @@ var invN2 = n1;
 
 alert("Antes N1 = "+n1+" | Agora n1 vale = "+invN1+"\n"+
 "Antes N2 = "+n2+" | Agora n2 vale = "+invN2)
+*/
 
+
+    //EXERCICIO 5
+var n1 = 70;
+var n2 = 9;
+var n3 = 800;
+
+if(n1 > n2 && n1 > n3){
+    var maior = n1;
+    if(n2 < n3){
+        var menor = n2
+    } else {var menor = n3}
+    alert ("Maior número: "+maior+"\n"+
+    "menor numero: "+menor)
+
+} else if ( n2 > n1 && n2 > n3 ){
+    var maior = n2;
+    if(n1 < n3){
+        var menor = n1
+    } else {var menor = n3}
+    alert ("Maior número: "+maior+"\n"+
+    "menor numero: "+menor)
+} 
+
+else  {
+    maior = n3;
+    if(n1 < n2){
+        var menor = n1
+    } else {var menor = n2}
+    alert ("Maior número: "+maior+"\n"+
+    "menor numero: "+menor)
+}

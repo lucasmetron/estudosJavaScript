72b86e19f80ee322f20b3a0d5a3a10abecf00cdf
lucasmetron <lucasmetron@gmail.com>
2021-02-18 10:27:53 -0300

    Exercicio IF encontrar vogal e consoante

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-function calcmedia (){
-    return (this.notas[0] + this.notas[1]) / 2
+var letra = "z";
+
+if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"  ){
+    console.log("Letra ("+letra+") é uma vogal!")
 }
+ else {
+    console.log("Letra ("+letra+") é uma consoante!")
+ }
 
-var aluno = {
-    nome:"Igor",
-    notas: [7,8],
-    media: calcmedia
+ 
 
-}
-var aluno1 = {
-    nome:"Lucas",
-    notas: [9, 5],
-    media: calcmedia
-    }
-
-
-console.log(aluno);
-
-console.log(aluno.nome);
-console.log(aluno.media());
-
-console.log(aluno1.nome)
-console.log(aluno1.media())
